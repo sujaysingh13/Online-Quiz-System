dbe98e6c6e12aecc22356ac55454c812eb277df6
Sujay Singh <sujaysingh1310@gmail.com>
2026-07-23 17:20:19 +0530

    ui changes

--- admin_app/views.py
+++ admin_app/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+
+from quizSystem.models import Test, Question, Result
+
+
+def is_admin(user):
+    return user.is_staff
+
+
+@user_passes_test(is_admin)
 def admin_app_index(request):
-    return render(request, 'admin_app/index.html')
+
+    total_students = User.objects.filter(
+        is_staff=False,
+        is_superuser=False
+    ).count()
+
+    total_tests = Test.objects.count()
+
+    total_questions = Question.objects.count()
+
+    total_attempts = Result.objects.count()
+
+    context = {
+
+        "total_students": total_students,
+
+        "total_tests": total_tests,
+
+        "total_questions": total_questions,
+
+        "total_attempts": total_attempts,
+
+    }
+
+    return render(
+        request,
+        "admin_app/index.html",
+        context
+    )
